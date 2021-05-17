a093b0ead44c7b0ff5687b1c6c7add4dc39d93be
SanadaC <chiho.sanada@rakus-partners.co.jp>
2021-05-17 16:06:43 +0900

    演習３完了

--- jquery3.js
+++ jquery3.js
@@ -1,3 +1,27 @@
-$("#show_btn").on("click", function(){
-   $("#text").show();
-  });
+'use strict';
+$(function () {
+    $('#label').hide();
+    $('#label2').hide();
+    $('#like').hide();
+    $('#like2').hide();
+
+    $('#select').change(function () {
+      var val = $(this).val();
+
+        switch (val) {
+            case '1':
+
+                $('#label').show();
+                $('#label2').show();
+                $('#like').hide();
+                $('#like2').hide();
+                break;
+            case '2':
+
+                $('#like').show();
+                $('#like2').show();
+                $('#label').hide();
+                $('#label2').hide();
+        }
+    });
+});
